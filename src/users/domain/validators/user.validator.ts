fc98802981350532b939a1fd4689784b33cc2159
Erick Leandro <j.erickleandro@gmail.com>
2024-09-23 20:57:33 +0000

    tests: user validations

--- src/users/domain/validators/user.validator.ts
+++ src/users/domain/validators/user.validator.ts
@@ -1,19 +1,46 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 import { UserProps } from '../entities/user.entity';
+import { ClassValidatorFields } from '../../../shared/domain/validators/class-validator-fields';
 
-class UserRules {
+export class UserRules {
   @MaxLength(255)
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   email: string;
 
+  @MaxLength(100)
+  @IsString()
+  @IsNotEmpty()
   password: string;
 
-  createdAt: Date;
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date;
 
   constructor({ email, name, password, createdAt }: UserProps) {
     Object.assign(this, { email, name, password, createdAt });
+  }
+}
+
+export class UserValidator extends ClassValidatorFields<UserRules> {
+  validate(data: UserProps): boolean {
+    return super.validate(new UserRules(data ?? ({} as UserProps)));
+  }
+}
+
+export class UserValidatorFactory {
+  static create(): UserValidator {
+    return new UserValidator();
   }
 }
