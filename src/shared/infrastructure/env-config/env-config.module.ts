ab3aa7fc3eaf9b20707f3e114661e840366d6c33
Erick Leandro <j.erickleandro@gmail.com>
2024-09-25 15:55:04 +0000

    fix: adjustment imports module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -4,10 +4,12 @@ import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { join } from 'node:path';
 
 @Module({
+  imports: [ConfigModule],
   providers: [EnvConfigService],
+  exports: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
-  static forRoot(options?: ConfigModuleOptions): DynamicModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
     return super.forRoot({
       ...options,
       envFilePath: [
